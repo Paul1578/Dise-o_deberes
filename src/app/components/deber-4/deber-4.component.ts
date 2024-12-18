94c13336df3f8a39b4ac00c9ce2710d9b8b69a56
unknown <lpa.carrillo@yavirac.edu.ec>
2024-12-17 20:20:18 -0500

    forms

--- src/app/components/deber-4/deber-4.component.ts
+++ src/app/components/deber-4/deber-4.component.ts
@@ -15,27 +15,29 @@ interface AutoCompleteCompleteEvent {
   styleUrl: './deber-4.component.css'
 })
 export class Deber4Component {
-  profileForm: FormGroup;
+  reservationForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.profileForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+    this.reservationForm = this.fb.group({
+      fullName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      cedula: ['', Validators.required],
-      opinion: ['', Validators.required],
-      direccion: ['', Validators.required],
-      gender: ['', Validators.required],
-      message: ['', Validators.required],
-      agree: [false, Validators.requiredTrue],
+      phone: ['', Validators.required],
+      checkIn: ['', Validators.required],
+      checkOut: ['', Validators.required],
+      roomType: ['', Validators.required],
+      guests: [1, [Validators.required, Validators.min(1)]],
+      comments: [''],
+      terms: [false, Validators.requiredTrue],
     });
   }
 
-  onSubmit(): void {
-    if (this.profileForm.valid) {
-      console.log(this.profileForm.value);
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log('Formulario Enviado:', this.reservationForm.value);
+      alert('¡Reservación realizada con éxito!');
+      this.reservationForm.reset();
     } else {
-      console.log('Formulario no válido');
+      alert('Por favor completa todos los campos requeridos.');
     }
   }
 }
